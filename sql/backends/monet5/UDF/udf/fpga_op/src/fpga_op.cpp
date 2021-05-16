57dfa0cc3607730a52b4d20693fe7737ff61bb8e
Runbin Shi <runbin.shi@inf.ethz.ch>
2021-05-16 18:21:59 +0200

    Feat: UDFdoubleit with FPGA sw_emu + hw_emu
    - add kernel_fpga_op
    - udf send one data to fpga and double it and send back
    - with sw_emu and hw_emu

--- sql/backends/monet5/UDF/udf/fpga_op/src/fpga_op.cpp
+++ sql/backends/monet5/UDF/udf/fpga_op/src/fpga_op.cpp
@@ -37,12 +37,168 @@
 #include "xcl2.hpp"
 #include "fpga_op.h"
 
+#define NUM_KERNEL 1
+
+// HBM Pseudo-channel(PC) requirements
+#define MAX_HBM_PC_COUNT 32
+#define PC_NAME(n) n | XCL_MEM_TOPOLOGY
+const int pc[MAX_HBM_PC_COUNT] = {
+        PC_NAME(0),  PC_NAME(1),  PC_NAME(2),  PC_NAME(3),  PC_NAME(4),  PC_NAME(5),  PC_NAME(6),  PC_NAME(7),
+        PC_NAME(8),  PC_NAME(9),  PC_NAME(10), PC_NAME(11), PC_NAME(12), PC_NAME(13), PC_NAME(14), PC_NAME(15),
+        PC_NAME(16), PC_NAME(17), PC_NAME(18), PC_NAME(19), PC_NAME(20), PC_NAME(21), PC_NAME(22), PC_NAME(23),
+        PC_NAME(24), PC_NAME(25), PC_NAME(26), PC_NAME(27), PC_NAME(28), PC_NAME(29), PC_NAME(30), PC_NAME(31)};
 
 extern "C"{
 
 str UDFdoubleit_(int *dst, const int *src)
 {
-    *dst = *src << 1;
+    unsigned int dataSize = 1;
+
+    //FIXME: fix the xclbin name
+        std::string binaryFile = "/home/runshi/Workspace/fpl20ext/MonetDB/build/build_dir.hw_emu.xilinx_u280_xdma_201920_3/krnl_fpga_op.xclbin";
+    cl_int err;
+    cl::CommandQueue q;
+    std::string krnl_name = "krnl_fpga_op";
+    std::vector<cl::Kernel> krnls(NUM_KERNEL);
+    cl::Context context;
+    std::vector<int, aligned_allocator<int> > source_in1(dataSize);
+    std::vector<int, aligned_allocator<int> > source_hw_double_results[NUM_KERNEL];
+
+    source_in1[0] = *src;
+
+    for (int i = 0; i < NUM_KERNEL; i++) {
+        source_hw_double_results[i].resize(dataSize);
+    }
+
+    // Initializing output vectors to zero
+    for (size_t i = 0; i < NUM_KERNEL; i++) {
+        std::fill(source_hw_double_results[i].begin(), source_hw_double_results[i].end(), 0);
+    }
+
+    // OPENCL HOST CODE AREA START
+    // The get_xil_devices will return vector of Xilinx Devices
+    auto devices = xcl::get_xil_devices();
+
+    // read_binary_file() command will find the OpenCL binary file created using
+    // the
+    // V++ compiler load into OpenCL Binary and return pointer to file buffer.
+    auto fileBuf = xcl::read_binary_file(binaryFile);
+
+    cl::Program::Binaries bins{{fileBuf.data(), fileBuf.size()}};
+
+    bool valid_device = false;
+    for (unsigned int i = 0; i < devices.size(); i++) {
+        auto device = devices[i];
+        // Creating Context and Command Queue for selected Device
+        OCL_CHECK(err, context = cl::Context(device, nullptr, nullptr, nullptr, &err));
+        OCL_CHECK(err, q = cl::CommandQueue(context, device,
+                                            CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE | CL_QUEUE_PROFILING_ENABLE, &err));
+
+        std::cout << "Trying to program device[" << i << "]: " << device.getInfo<CL_DEVICE_NAME>() << std::endl;
+        cl::Program program(context, {device}, bins, nullptr, &err);
+        if (err != CL_SUCCESS) {
+            std::cout << "Failed to program device[" << i << "] with xclbin file!\n";
+        } else {
+            std::cout << "Device[" << i << "]: program successful!\n";
+            // Creating Kernel object using Compute unit names
+
+
+            for (int i = 0; i < NUM_KERNEL; i++) {
+                std::string cu_id = std::to_string(i + 1);
+                std::string krnl_name_full = krnl_name + ":{" + "krnl_fpga_op_" + cu_id + "}";
+
+                printf("Creating a kernel [%s] for CU(%d)\n", krnl_name_full.c_str(), i + 1);
+
+                // Here Kernel object is created by specifying kernel name along with
+                // compute unit.
+                // For such case, this kernel object can only access the specific
+                // Compute unit
+
+                OCL_CHECK(err, krnls[i] = cl::Kernel(program, krnl_name_full.c_str(), &err));
+            }
+            valid_device = true;
+            break; // we break because we found a valid device
+        }
+    }
+    if (!valid_device) {
+        std::cout << "Failed to program any device found, exit!\n";
+        exit(EXIT_FAILURE);
+    }
+
+    std::vector<cl_mem_ext_ptr_t> inBufExt1(NUM_KERNEL);
+    std::vector<cl_mem_ext_ptr_t> outDoubleBufExt(NUM_KERNEL);
+
+
+    std::vector<cl::Buffer> buffer_input1(NUM_KERNEL);
+    std::vector<cl::Buffer> buffer_output_double(NUM_KERNEL);
+
+    // For Allocating Buffer to specific Global Memory PC, user has to use
+    // cl_mem_ext_ptr_t
+    // and provide the PCs
+    for (int i = 0; i < NUM_KERNEL; i++) {
+        inBufExt1[i].obj = source_in1.data();
+        inBufExt1[i].param = 0;
+        inBufExt1[i].flags = pc[i * 4];
+
+        outDoubleBufExt[i].obj = source_hw_double_results[i].data();
+        outDoubleBufExt[i].param = 0;
+        outDoubleBufExt[i].flags = pc[(i * 4) + 1];
+    }
+
+    // These commands will allocate memory on the FPGA. The cl::Buffer objects can
+    // be used to reference the memory locations on the device.
+    // Creating Buffers
+    for (int i = 0; i < NUM_KERNEL; i++) {
+        OCL_CHECK(err,
+                  buffer_input1[i] = cl::Buffer(context, CL_MEM_READ_ONLY | CL_MEM_EXT_PTR_XILINX | CL_MEM_USE_HOST_PTR,
+                                                sizeof(uint32_t) * dataSize, &inBufExt1[i], &err));
+        OCL_CHECK(err, buffer_output_double[i] =
+                               cl::Buffer(context, CL_MEM_WRITE_ONLY | CL_MEM_EXT_PTR_XILINX | CL_MEM_USE_HOST_PTR,
+                                          sizeof(uint32_t) * dataSize, &outDoubleBufExt[i], &err));
+    }
+
+    // Copy input data to Device Global Memory
+    for (int i = 0; i < NUM_KERNEL; i++) {
+        OCL_CHECK(err,
+                  err = q.enqueueMigrateMemObjects({buffer_input1[i]}, 0 /* 0 means from host*/));
+    }
+    q.finish();
+
+    double kernel_time_in_sec = 0, result = 0;
+
+    std::chrono::duration<double> kernel_time(0);
+
+    auto kernel_start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < NUM_KERNEL; i++) {
+        // Setting the k_vadd Arguments
+        OCL_CHECK(err, err = krnls[i].setArg(0, buffer_input1[i]));
+        OCL_CHECK(err, err = krnls[i].setArg(1, buffer_output_double[i]));
+        OCL_CHECK(err, err = krnls[i].setArg(2, dataSize));
+
+        // Invoking the kernel
+        OCL_CHECK(err, err = q.enqueueTask(krnls[i]));
+    }
+
+    q.finish();
+
+    auto kernel_end = std::chrono::high_resolution_clock::now();
+
+    kernel_time = std::chrono::duration<double>(kernel_end - kernel_start);
+
+    kernel_time_in_sec = kernel_time.count();
+    kernel_time_in_sec /= NUM_KERNEL;
+
+    // Copy Result from Device Global Memory to Host Local Memory
+    for (int i = 0; i < NUM_KERNEL; i++) {
+        OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_output_double[i]}, CL_MIGRATE_MEM_OBJECT_HOST));
+    }
+    q.finish();
+
+    bool match = true;
+
+    // write back to udf
+    *dst = source_hw_double_results[0][0];
+
     return MAL_SUCCEED;
 }
 
